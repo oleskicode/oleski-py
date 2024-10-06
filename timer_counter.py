d4f770818c4c1651b45b9c5bc9802d840dbafc4a
Oleksii Plaunov <plaunov@gmail.com>
2024-10-06 16:48:21 +0300

    Timer counter task added

---
+++ timer_counter.py
@@ -0,0 +1,14 @@
+import time
+
+inputSeconds = input("Set timer seconds:")
+# inputSeconds = 3663
+inputSeconds = int(inputSeconds)
+
+for x in range(inputSeconds, 0, -1):
+    seconds = x % 60
+    minutes = int(x/60) % 60
+    hours = int(x/3600)
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(0.3)
+
+print("Time's up!")
