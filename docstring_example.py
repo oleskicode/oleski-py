f2012dd697cb6b5cda75314bfef06e781b885c48
oleksii <plaunov@gmail.com>
2024-10-09 14:12:57 +0300

    doctring example

---
+++ docstring_example.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    """
+    This function adds x and y
+    :param x: 
+    :param y: 
+    :return: x+y
+    """
+    return(x+y)
+
+print(add(1,2))
+
+print(add.__doc__) #prints docstring
