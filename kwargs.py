24a0ebb9004a6b407f7b09ff647f1252c57cfdf9
oleksii <plaunov@gmail.com>
2024-10-09 13:10:40 +0300

    **kwargs example

---
+++ kwargs.py
@@ -0,0 +1,15 @@
+def print_address(**kwargs):
+    for value in kwargs.values():
+        print(value)
+    print("-----")
+
+print_address(city = "Dilly", street = "Dally 12" )
+print_address(street = "Happy",  house= "Clappy 10" )
+
+user_address = {
+    "street" : "Abbey St.",
+    "city" : "Cambridge",
+    "apartment" : 10
+}
+
+print_address(**user_address) # dict must be unpacked
