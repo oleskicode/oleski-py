f330be9ea15d41f55f58cc4526704b0ba944fbb3
oleksii <plaunov@gmail.com>
2024-10-09 11:53:47 +0300

    *args example added

---
+++ args.py
@@ -0,0 +1,11 @@
+def add_numbers(*args):
+    sum = 0
+    for number in args:
+        sum += number
+    print(sum)
+
+add_numbers()
+add_numbers(1)
+add_numbers(1,2)
+add_numbers(1,2,3)
+add_numbers(1,2,3,4)
