5a724d69429801e9478b3b1926c6ad587b55a314
oleksii <plaunov@gmail.com>
2024-10-12 18:26:41 +0300

    decorator.py

--- decorator.py
+++ decorator.py
@@ -1,11 +1,19 @@
-def printing(func):
+def printing_dashes(func):
     def wrapper():
         print("---beginning---")
         func()
         print("-----end-----")
     return wrapper
 
-@printing
+def printing_stars(func):
+    def wrapper():
+        print("***start***")
+        func()
+        print("***end***")
+    return wrapper
+
+@printing_dashes
+@printing_stars
 def say_hello():
     print("Hello")
 
