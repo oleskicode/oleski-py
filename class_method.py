ddcee25b046ad199fe63fcf229dc171ee4ddf983
oleksii <plaunov@gmail.com>
2024-10-12 23:15:27 +0300

    Static and Class methods

---
+++ class_method.py
@@ -0,0 +1,15 @@
+class Wolf:
+    wolf_counter = 0
+
+    def __init__(self):
+        self.number = Wolf.wolf_counter #assign number to wolf
+        Wolf.wolf_counter += 1 #update total wolf counter
+
+    @classmethod
+    def how_many_wolfs(cls):
+        return cls.wolf_counter
+
+wolf1 = Wolf()
+wolf2 = Wolf()
+
+print(Wolf.how_many_wolfs())
---
+++ static_method.py
@@ -0,0 +1,13 @@
+class Wolf:
+    breeds = ["Northern", "White", "Brown", "American"]
+
+    @staticmethod
+    def is_known_breed(breed):
+        if breed in Wolf.breeds:
+            print(f"{breed} is a known breed")
+        else:
+            print(f"{breed} is not a known breed")
+
+# Calling the static method with known and unknown breeds
+Wolf.is_known_breed("Brown")   # Output: Brown is a known breed
+Wolf.is_known_breed("Yellow")  # Output: Yellow is not a known breed
