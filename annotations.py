54a4a04fc589fd52487ee747c6bbc0e712a83c3e
oleksii <plaunov@gmail.com>
2024-10-14 11:57:42 +0300

    annotations and string-list convert

---
+++ annotations.py
@@ -0,0 +1,6 @@
+def hello_username(username: str) -> str:
+    return f"Hello {username}"
+
+username = "Bob"
+
+print(hello_username(username))
---
+++ string_to_list_list_to_string.py
@@ -0,0 +1,8 @@
+list1 = ['some','l','i','s','t']
+to_string = "".join(list1)
+print(to_string) # somelist
+
+string1 = "some string"
+to_list = list(string1)
+print(to_list) # ['s', 'o', 'm', 'e', ' ', 's', 't', 'r', 'i', 'n', 'g']
+
