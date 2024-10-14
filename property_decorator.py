fc1dd66f3f822d8bae4ecfb1b8a8ec4b8b13ef1d
oleksii <plaunov@gmail.com>
2024-10-14 14:47:30 +0300

    property_decorator

---
+++ property_decorator.py
@@ -0,0 +1,13 @@
+class Tractor():
+    def __init__(self, color):
+        self.color = color
+        self.wheels = 4
+
+    @property
+    def get_color(self):
+        return self.color
+
+
+trackie = Tractor("green")
+
+print(trackie.get_color)  # without parentheses () since get_color is a property, not a method.
