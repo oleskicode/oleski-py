9b58e5d5457959cba43263c93c09d9cf6cce7985
oleksii <plaunov@gmail.com>
2024-10-14 17:22:09 +0300

    generator

---
+++ generator.py
@@ -0,0 +1,10 @@
+def file_line_printer(file):
+    with open(file, 'r') as f: # Use 'f' as the file object
+        for file_line in f:
+            # print(line.strip())
+            yield file_line
+
+file_path = "generator_sample_file.txt"
+
+for line in file_line_printer(file_path):
+    print(line.strip())
