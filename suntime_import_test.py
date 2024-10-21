f2b6b862559ca2ce7177fd571990d75a65a0b7eb
oleksii <plaunov@gmail.com>
2024-10-21 15:23:44 +0300

    suntime_import_test.py

---
+++ suntime_import_test.py
@@ -0,0 +1,16 @@
+from colorama import Fore, Back
+from suntime import Sun, SunTimeException # pip3 install suntime
+
+city_name = "Lviv"
+city_lat = 49.83
+city_lon = 23.98
+
+sun = Sun(city_lat, city_lon)
+
+# Get today's sunrise and sunset in UTC
+today_sr = sun.get_sunrise_time()
+today_ss = sun.get_sunset_time()
+print(f"Today in {city_name}:")
+print(Fore.YELLOW + Back.BLACK + f"sunrise time: {today_sr} UTC")
+print(Fore.BLUE + Back.BLACK + f"sunset  time: {today_ss} UTC")
+
