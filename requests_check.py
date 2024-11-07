f2c6748e536da80a13908f42ad3da86543f022d2
oleksii <plaunov@gmail.com>
2024-11-07 22:20:19 +0200

    requests_check.py

---
+++ requests_check.py
@@ -0,0 +1,26 @@
+import requests
+
+base_url = "https://api.stackexchange.com"
+print(base_url)
+
+response = requests.get(base_url + '/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+print(f"Status Code: {response.status_code} \n")
+
+print(f"response.headers ${response.headers} \n")
+for key_header, value_header in response.headers.items():
+    print(f"{key_header}: {value_header}")
+
+with open("response.json", "w") as file:
+    file.write(str(response.json()))
+
+# print(type(response))
+# print(type(response.headers))
+# print(type(response.json()))
+# print(type(response.json()['items']))
+# print(type(response.connection))
+# print(type(response.status_code))
+
+for item in response.json()['items']:
+    print(item['title'])
+    print(item['link'])
+
