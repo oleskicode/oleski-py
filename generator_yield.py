19ddfa603c98cbd1f1638e7e670ec0db92bf439c
oleksii <plaunov@gmail.com>
2024-11-14 11:15:12 +0200

    generator_yield.py

---
+++ generator_yield.py
@@ -0,0 +1,13 @@
+def my_generator():
+    print("First part")
+    yield 1
+    print("After first yield")
+    yield 2
+    print("After second yield")
+    yield 3
+
+gen = my_generator()
+
+print(next(gen))  # Outputs 1, prints "First part"
+print(next(gen))  # Outputs 2, prints "After first yield"
+print(next(gen))  # Outputs 3, prints "After second yield"
