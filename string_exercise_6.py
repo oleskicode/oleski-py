1dcc02497a7d08d6cf26a4b42c3ada81b89fa3bf
Oleksii Plaunov <plaunov@gmail.com>
2025-03-03 18:04:32 +0200

    string exercise

---
+++ string_exercise_6.py
@@ -0,0 +1,15 @@
+# Count how many VOWEL characters are in the string
+vowels = ["a","A","e","E","i","I","o","O","u","U"]
+# print(vowels)
+
+def count_vowels(str_input: str) -> int:
+    vowels_count = 0
+    for char in str_input:
+        if char in vowels:
+            # print(f"{char} is a vowel")
+            vowels_count += 1
+    return vowels_count
+
+print(count_vowels("abc"))
+print(count_vowels("defghij"))
+print(count_vowels("klm"))
