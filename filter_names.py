fb9675b101fa729ab2ff9a3cf27166adf2ceb084
Oleksii Plaunov <plaunov@gmail.com>
2025-03-23 18:58:15 +0200

    filter names exercise

---
+++ filter_names.py
@@ -0,0 +1,11 @@
+# Filter list to include names which have "a"
+
+def filter_list_with_symbol(input_list: list, symbol: str) -> list:
+    def check_symbol_in_string(string1: str):
+        return symbol.lower() in string1.lower()
+
+    return list(filter(check_symbol_in_string,input_list))
+
+print(filter_list_with_symbol(["Anne", "Bob", "Carol", "Danny"],"a"))
+print(filter_list_with_symbol(["Anne", "Bob", "Carol", "Danny"],"b"))
+print(filter_list_with_symbol(["Anne", "Bob", "Carol", "Danny"],"c"))
