95b0e83989229391449332e5ea632e618b6accc4
Oleksii Plaunov <plaunov@gmail.com>
2025-04-01 10:29:12 +0300

    dict_merge_lists and list_comprehension exercise

---
+++ list_comprehension_fruits.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "coconut", "donut"]
+
+def capitalize_each_word_in_list(input_list: list) -> list:
+    capitalized_list = [item.capitalize() for item in input_list]
+    return capitalized_list
+
+print(capitalize_each_word_in_list(fruits))
