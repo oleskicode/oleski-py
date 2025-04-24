6e5509834edcd3f788b513c6225488589fbeec18
Oleksii Plaunov <plaunov@gmail.com>
2025-04-24 21:29:49 +0300

    palindrome int

---
+++ palindrome_number.py
@@ -0,0 +1,19 @@
+# Palindrome Number
+# Given an integer `x`, return `true` if `x` is a palindrome, and `false` otherwise.
+# #### Example 1:
+# **Input:** `x = 121`
+# **Output:** `true`
+# **Explanation:** 121 reads as 121 from left to right and from right to left.
+# #### Example 2:
+# **Input:** `x = -121`
+# **Output:** `false`
+# **Explanation:** From left to right, it reads -121. From right to left, it becomes 121-. Therefore, it is not a palindrome.
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+
+print(Solution.isPalindrome(Solution(),121))
+print(Solution.isPalindrome(Solution(),-121))
