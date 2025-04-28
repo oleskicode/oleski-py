92d13ccdd3bc44764aabdd34ac86649d36050317
Oleksii Plaunov <plaunov@gmail.com>
2025-04-28 23:28:20 +0300

    str dict char counter

---
+++ string_dict_counter.py
@@ -0,0 +1,14 @@
+def str_to_dict_count(s: str) -> dict:
+    """Count characters in a string"""
+    result_dict = {}
+    for char in s:
+        if char in result_dict:
+            result_dict[char] += 1
+        else:
+            result_dict[char] = 1
+    return result_dict
+
+print(str_to_dict_count.__doc__)
+print(str_to_dict_count("apple"))
+print(str_to_dict_count("banana"))
+print(str_to_dict_count("coconut"))
